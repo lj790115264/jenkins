c246f718a27103e4813550f314f693fb1426b43a
fan <1452125537@qq.com>
2017-12-19 17:22:30 +0800

    Initial commit

---
+++ src/main/java/com/chinacaring/hmsrmyy/HmsrmyyApplication.java
@@ -0,0 +1,12 @@
+package com.chinacaring.hmsrmyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsrmyyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsrmyyApplication.class, args);
+	}
+}
