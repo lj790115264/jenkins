189c93ce3c463ecd68d4fb26161657e7717505d6
fan <1452125537@qq.com>
2018-01-04 11:24:36 +0800

    添加了 ping pp 依赖对应的仓库 其他都是瞎逼操作

--- src/main/java/com/chinacaring/hmsrmyy/HmsrmyyApplication.java
+++ src/main/java/com/chinacaring/hmsrmyy/HmsrmyyApplication.java
@@ -3,6 +3,7 @@ package com.chinacaring.hmsrmyy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = {"com.chinacaring"})
 @MapperScan({"com.chinacaring.user.mapper", "com.chinacaring.user.dao.mapper"})
