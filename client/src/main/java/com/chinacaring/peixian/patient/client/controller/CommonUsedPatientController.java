7cd5b3ba81134b00eea90c1250acf81595b8812b
lanjian <790115264@qq. com>
2018-02-12 10:50:42 +0800

    调试接口

--- client/src/main/java/com/chinacaring/peixian/patient/client/controller/CommonUsedPatientController.java
+++ client/src/main/java/com/chinacaring/peixian/patient/client/controller/CommonUsedPatientController.java
@@ -3,20 +3,27 @@ package com.chinacaring.peixian.patient.client.controller;
 
 import com.chinacaring.common.exception.CommonException;
 import com.chinacaring.common.vo.Result;
+import com.chinacaring.peixian.patient.client.config.MyGlobalExceptionHandler;
 import com.chinacaring.peixian.patient.client.dto.front.request.CommonUsedPatientRequest;
+import com.chinacaring.peixian.patient.client.dto.front.response.CommonUsedPatientResponse;
 import com.chinacaring.peixian.patient.client.service.CommonUsedPatientService;
 import com.chinacaring.user.annotation.CurrentUser;
 import com.chinacaring.user.dao.entity.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
+import java.util.List;
 
 @RestController
 @Api("常用就诊人接口")
 public class CommonUsedPatientController {
+
+    private Logger logger = LoggerFactory.getLogger(CommonUsedPatientController.class);
 
     @Autowired
     private CommonUsedPatientService commonUsedPatientService;
@@ -42,7 +49,14 @@ public class CommonUsedPatientController {
     @GetMapping("/patient")
     public Object getCommonUsedPatient(@CurrentUser User user) throws CommonException {
 
-        return new Result<>(commonUsedPatientService.getCommonUsedPatient(user));
+        Long begin = System.currentTimeMillis();
+        List<CommonUsedPatientResponse> list = commonUsedPatientService.getCommonUsedPatient(user);
+        Long end = System.currentTimeMillis();
+
+        logger.info("总共花了-------------");
+        logger.info(String.valueOf(end - begin));
+
+        return new Result<>(list);
 
     }
 
