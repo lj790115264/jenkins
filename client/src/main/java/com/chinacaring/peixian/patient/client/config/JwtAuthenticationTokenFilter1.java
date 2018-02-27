76fc6a1675bb51595ce96fd96d5301857b6d4e11
lanjian <790115264@qq. com>
2018-02-27 11:08:18 +0800

    测试

---
+++ client/src/main/java/com/chinacaring/peixian/patient/client/config/JwtAuthenticationTokenFilter1.java
@@ -0,0 +1,41 @@
+package com.chinacaring.peixian.patient.client.config;
+
+import com.chinacaring.auth.utils.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationTokenFilter1 extends OncePerRequestFilter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Value("${jwt.header}")
+    private String tokenHeader;
+    @Value("${jwt.tokenHead}")
+    private String tokenHead;
+
+    public JwtAuthenticationTokenFilter1() {
+    }
+
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        Long time2 = System.currentTimeMillis();
+        logger.error(String.valueOf(time2));
+
+        chain.doFilter(request, response);
+    }
+}
