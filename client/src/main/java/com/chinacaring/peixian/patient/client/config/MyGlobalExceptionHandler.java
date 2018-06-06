ab116682b5f4c3e8f08ec63dbbde48f41e2e8103
兰健 <790115264@qq.com>
2018-06-06 17:53:59 +0800

    debug

--- client/src/main/java/com/chinacaring/peixian/patient/client/config/MyGlobalExceptionHandler.java
+++ client/src/main/java/com/chinacaring/peixian/patient/client/config/MyGlobalExceptionHandler.java
@@ -24,6 +24,7 @@ public class MyGlobalExceptionHandler extends GlobalExceptionHandler {
 
     @ExceptionHandler({NullPointerException.class})
     public Object nullExceptionHandler(HttpServletRequest req, NullPointerException e){
+        logger.error(e.getMessage());
         return new Result<>(ResultStatusCode.SYSTEM_ERR.getErrorCode(), e.toString(),  "接口异常",null);
     }
 
