87cc5ef627b9823adeacfb8a63cfcd189a49701b
兰健 <790115264@qq.com>
2018-06-06 17:45:08 +0800

    chongxie exception

--- client/src/main/java/com/chinacaring/peixian/patient/client/config/MyGlobalExceptionHandler.java
+++ client/src/main/java/com/chinacaring/peixian/patient/client/config/MyGlobalExceptionHandler.java
@@ -8,9 +8,11 @@ import com.chinacaring.peixian.patient.client.exception.MyException;
 import com.chinacaring.peixian.patient.client.exception.SoapException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -28,6 +30,28 @@ public class MyGlobalExceptionHandler extends GlobalExceptionHandler {
     @ExceptionHandler({CommonException.class})
     public Object commonExceptionHandler(HttpServletRequest req, CommonException e){
         return new Result<>( ResultStatusCode.WEBSERVICE_WRONG_RESPONSE.getErrorCode(), e.getDetailMessage(), "接口返回码 非 1", null);
+    }
+
+    @ExceptionHandler({Exception.class})
+    public Result defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        Result result = new Result();
+        result.setMessage(e.getMessage());
+        if (e instanceof NoHandlerFoundException) {
+            result.setCode(404);
+        } else if (e instanceof CommonException) {
+            result.setCode(((CommonException)e).getResultCode());
+        } else {
+            result.setCode(500);
+        }
+
+        result.setData((Object)null);
+        if (!(e instanceof NoHandlerFoundException) && !(e instanceof MissingServletRequestParameterException)) {
+            this.sendEmail(req, e);
+        }
+
+        logger.error(e.getMessage());
+
+        return result;
     }
 
     @ExceptionHandler({MyException.class})
